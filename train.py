abb0966ad45fa8152f776583cae2b2afd0998934
Maverick-2000 <stevebobygeorge@gmail.com>
2022-05-02 15:24:19 +0530

    Fix graph gen function & add .gitignore

--- train.py
+++ train.py
@@ -4,7 +4,8 @@ from sys import argv
 from pathlib import Path
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+
 
 import keras
 from keras.models import Sequential
@@ -25,11 +26,12 @@ POINT_DIM = 3
 # hand_model3: (0.3) 86%
 def build_model(labels, frame_dim, dropout=0.3):
     model = Sequential()
-    model.add(keras.Input(shape = (TIMESTEPS, frame_dim)))
-    model.add(layers.LSTM(128, name="lstm1", dropout=dropout, return_sequences=True))
+    model.add(keras.Input(shape=(TIMESTEPS, frame_dim)))
+    model.add(layers.LSTM(128, name="lstm1",
+              dropout=dropout, return_sequences=True))
     model.add(layers.LSTM(64, name="lstm2", dropout=dropout))
     model.add(layers.Dense(labels, activation="softmax"))
-    adam = Adam(lr = 0.0002)
+    adam = Adam(lr=0.0002)
     model.compile(loss='categorical_crossentropy',
                   optimizer=adam,
                   metrics=['accuracy'])
@@ -42,6 +44,7 @@ def get_labels(dirname):
     nonholds = [sign.name for sign in Path(dirname, 'nonholds_data').iterdir()]
     return (sorted(holds + nonholds), set(holds))
 
+
 def load_data(dirname):
     for sign in Path(dirname, 'holds_data').iterdir():
         for datafile in sign.iterdir():
@@ -50,20 +53,25 @@ def load_data(dirname):
         for datafile in sign.iterdir():
             yield (holistic.read_datafile(datafile), sign.name)
 
+
 def count_labels(data_iter, count):
     for data, sign in iter(data_iter):
         count[sign] += 1
         yield (data, sign)
+
+
 def count_zeros(data_iter, count):
     for data, sign in iter(data_iter):
         if sign == 0:
             count[None] += 1
         yield (data, sign)
 
+
 def label_signs(data_iter, labels):
-    labels = {l:i+1 for i, l in enumerate(labels)}
+    labels = {l: i+1 for i, l in enumerate(labels)}
     for data, sign in iter(data_iter):
         yield (data, labels[sign])
+
 
 def onehot_labelled_signs(data_iter, num_labels):
     for data, sign in iter(data_iter):
@@ -71,10 +79,12 @@ def onehot_labelled_signs(data_iter, num_labels):
         onehot[sign] = 1
         yield (data, onehot)
 
+
 def generate_windows(data, sign, start, end, stride):
     while start + TRAIN_TIMESTEPS < min(end, data.shape[0]):
         yield (data[start:start+TRAIN_TIMESTEPS], sign)
         start += stride
+
 
 def truncate_and_divide_data(data_iter, holds, trim):
     for data, sign in iter(data_iter):
@@ -91,18 +101,22 @@ def truncate_and_divide_data(data_iter, holds, trim):
         else:
             yield (data, sign)
 
+
 def extend_data(data_iter):
     for data, sign in iter(data_iter):
         if data.shape[0] < TRAIN_TIMESTEPS:
             #print("{} {}".format(sign, data.shape[0]))
-            zeros = np.zeros( (TRAIN_TIMESTEPS-data.shape[0],) + data.shape[1:] )
+            zeros = np.zeros((TRAIN_TIMESTEPS-data.shape[0],) + data.shape[1:])
             data = np.concatenate((data, zeros), axis=0)
         yield (data, sign)
 
+
 def add_gesture_zero(dataset, num_labels):
     len_per_label = int(len(dataset) // num_labels)
-    zeros = [(np.zeros((TRAIN_TIMESTEPS, dataset[0][0].shape[1])), 0)] * len_per_label
+    zeros = [(np.zeros((TRAIN_TIMESTEPS, dataset[0][0].shape[1])), 0)
+             ] * len_per_label
     return dataset + zeros
+
 
 def add_none_data(data_iter, dirname):
     for data, sign in iter(data_iter):
@@ -111,6 +125,7 @@ def add_none_data(data_iter, dirname):
         data = holistic.read_datafile(datafile)
         yield from generate_windows(data, 0, 0, data.shape[0], TRAIN_TIMESTEPS)
 
+
 def speed_up_data(data_iter):
     for data, sign in iter(data_iter):
         data = data[::2]
@@ -118,6 +133,8 @@ def speed_up_data(data_iter):
 
 
 TEST_SPLIT = 3
+
+
 def split_data(data_iter):
     dataset, dataset_test = [], []
     for i, (data, sign) in enumerate(data_iter):
@@ -128,6 +145,7 @@ def split_data(data_iter):
             dataset_ref = dataset
         dataset_ref.append((data, sign))
     return (dataset, dataset_test)
+
 
 def load_and_process_data(dirname):
     labels, holds = get_labels(dirname)
@@ -140,7 +158,8 @@ def load_and_process_data(dirname):
     data_iter = count_labels(data_iter, count)
     data_iter = label_signs(data_iter, labels)
     data_iter = add_gesture_zero(list(data_iter), len(labels))
-    data_iter = add_none_data(data_iter, dirname) # Comment out this line to exclude "None" data when training
+    # Comment out this line to exclude "None" data when training
+    data_iter = add_none_data(data_iter, dirname)
     data_iter = count_zeros(data_iter, count)
     data_iter = onehot_labelled_signs(data_iter, len(labels))
     data_iter = speed_up_data(data_iter)
@@ -161,31 +180,54 @@ def load_and_process_data(dirname):
 
     return X, Y, X_test, Y_test
 
-def plot_data(history, name1, name2):
-    plt.figure()
-    plt.plot(history[name1], label=name1)
-    plt.plot(history[name2], label=name2)
-    plt.legend()
+
+def plot_data_acc(history):
+
+    plt.plot(history['accuracy'])
+    plt.plot(history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['TRAIN', 'VAL'], loc='lower right')
+    plt.savefig('acc.png', bbox_inches='tight')
+    plt.clf()
+
+def plot_data_loss(history):
+
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['TRAIN', 'VAL'], loc='lower right')
+    plt.savefig('loss.png', bbox_inches='tight')
+    plt.clf()
+
 
 def train_model(dirname, epochs=25, batch_size=128, val_split=0.25):
     X, Y, X_test, Y_test = load_and_process_data(dirname)
-    print("Size of training set = {}, test set = {}".format(X.shape[0], X_test.shape[0]))
+    print("Size of training set = {}, test set = {}".format(
+        X.shape[0], X_test.shape[0]))
     model = build_model(Y.shape[1], X.shape[2])
-    history = model.fit(X, Y, epochs=epochs, batch_size=batch_size, validation_split=val_split)
+    history = model.fit(X, Y, epochs=epochs,
+                        batch_size=batch_size, validation_split=val_split)
+    plot_data_loss(history.history)
+    plot_data_acc(history.history)
     score, acc = model.evaluate(X_test, Y_test, batch_size=batch_size)
 
     print("score: {} accuracy: {}".format(score, acc))
-    plot_data(history.history, 'loss', 'val_loss')
-    plot_data(history.history, 'accuracy', 'val_accuracy')
-    plt.show()
+
     return model
+
 
 def init_gpu():
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
+
 # Usage: [data_dir] [model_folder_name]
+#eg: train.py output model
 if __name__ == "__main__":
     data_dir = argv[1]
     init_gpu()
